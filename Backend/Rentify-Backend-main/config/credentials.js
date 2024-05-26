d1671c1dabf5aee99a9214206c0c6eec2a439c78
The-Akhilesh-Thapliyal <akhileshthapliyal20@gmail.com>
2024-05-26 17:33:51 +0530

    Rentify MERN Full Stack Application

---
+++ Backend/Rentify-Backend-main/config/credentials.js
@@ -0,0 +1,13 @@
+const { mongo } = require("mongoose");
+
+const mongoURI = "mongodb+srv://akhileshthapliyal20:b98Ic6z746xdQWY8@rentifyclustera.s0dphl2.mongodb.net/?retryWrites=true&w=majority&appName=RentifyClusterA";
+
+const email = "akhileshthapliyal20@gmail.com";
+
+const password = "mrra xyur tedl ernc";
+
+module.exports = {
+    mongoURI,
+    email,
+    password
+};
---
+++ Frontend/src/config.api.js
@@ -0,0 +1,4 @@
+// const API = "http://localhost:8000/api";
+const API = 'https://rentify-backend-p7nl.onrender.com/api';
+
+module.exports = API;
